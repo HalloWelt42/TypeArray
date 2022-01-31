130a7b5698377335186e0130e9f41011db0579c2
HalloWelt42 <github@texeln.de>
2022-01-31 01:07:09 +0100

    feat: demoproject as an example

---
+++ src/demo/Main.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Hallowelt42\TypeArray\demo;
+
+class Main
+{
+    public function __construct()
+    {
+
+        // create
+        $persons = new PersonList();
+
+        $person = new Person();
+        $person->setId(1);
+        $person->setName('Alice');
+        $persons[] = $person;
+
+        $person = new Person();
+        $person->setId(2);
+        $person->setName('Bob');
+        $persons[] = $person;
+
+        $manager = new PersonManager($persons);
+        $manager->printAll();
+
+        /**
+         * this code print:
+         *
+         *      ID: 1 - Name: Alice
+         *      ID: 2 - Name: Bob
+         *
+         *
+         */
+
+    }
+}
---
+++ src/demo/index.php
@@ -0,0 +1,7 @@
+<?php
+
+use Hallowelt42\TypeArray\demo\Main;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+new Main();
